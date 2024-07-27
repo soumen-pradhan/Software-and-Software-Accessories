d55296c46f2b3033df632ff38beeb096ae5442cc
soumen-pradhan <sam.thepradhans@gmail.com>
2024-07-27 22:25:24 +0530

    bugfix: make trailing slashes uniform

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://software-and-software-accessories.netlify.app',
+  // site: 'https://software-and-software-accessories.netlify.app',
+  trailingSlash: 'always',
   redirects: {
     '/': '/about',
   },
